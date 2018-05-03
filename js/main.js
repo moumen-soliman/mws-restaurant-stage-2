9b4dc1a982e564bce6ce9587efcc97531267a710
Mo'men Soliman <moumensoliman@gmail.com>
2018-05-04 00:17:32 +0200

    Fetch neighborhoods and cuisines

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let restaurants,
+  neighborhoods,
+  cuisines
+var map
+var markers = []
+
+/**
+ * Fetch neighborhoods and cuisines as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetchNeighborhoods();
+  fetchCuisines();
+});
